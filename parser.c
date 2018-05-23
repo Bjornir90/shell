77353e47cd8bde9d280d331e8458030c1cbfef3c
Jules Paget <jules25115@hotmail.fr>
2018-05-23 14:11:35 +0200

    allowing spaces between brackets in parser

--- parser.c
+++ parser.c
@@ -4,20 +4,40 @@
 #define STRSIZE 1024
 
 int getNextArgument(int index, char * input, char * argument, char separator){
-	char iterator = input[index];
-	int indexArgument = 0;
-	if(iterator == '\0' || strcmp(input, "") == 0){//If the input is empty
-		argument = "";
-		return -1;
-	}
-	while(iterator != separator && iterator != '\n'){//Loop to the end of the word
-		argument[indexArgument] = iterator;//Build argument string
-		indexArgument++;
-		index++;
-		iterator = input[index];
-	}
-	argument[indexArgument] = '\0';
-	return index;
+  char iterator = input[index];
+  int indexArgument = 0;
+  if(iterator == '\0' || strcmp(input, "") == 0){//If the input is empty
+    argument = "";
+    return -1;
+  }
+  while(iterator != separator && iterator != '\n'){//Loop to the end of the word
+    if (iterator == '"'){ //If you find a guard symbol ("), it allows you to have spaces until the next one
+        //1st iteration to skip the 1st ".
+        index++;
+        iterator = input[index];
+        //printf("1ere iteration après un ' \n");
+        while(iterator != '"' && iterator != '\n'){
+          //printf("iterator = %c \n", iterator );
+          argument[indexArgument] = iterator;//Build argument string
+          indexArgument++;
+          index++;
+          iterator = input[index];
+        }
+        //last iteration to skip the last ".
+        index++;
+        iterator = input[index];
+        //printf("derniere iteration après un ' \n");
+    }
+    else{
+      argument[indexArgument] = iterator;//Build argument string
+      indexArgument++;
+      index++;
+      iterator = input[index];
+    }
+  }
+  argument[indexArgument] = '\0';
+  //printf("argument = %s \n", argument);
+  return index;
 }
 
 int getAllArguments(int index, char * input, char ** result, char separator){
